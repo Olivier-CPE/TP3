369a2d0df8ee4b55e410831e819e6fbe6e143573
Olivier Mermet <omermet004@gmail.com>
2023-10-05 09:33:57 +0200

    sauvegarde

--- fonctions.py
+++ fonctions.py
@@ -1,10 +1,26 @@
 ## Olivier et Maxime 
 
 
+import random as rd
+
+def recuperer_dictionnaire():
+    """
+        On recupère un fichier de mots on en fait une liste 
+    """
+    fichier = open("assets\dictionnaire.txt", "r")
+    temp = fichier.read()
+    dico_mots = temp.split()
+    # conserve uniquement les mots de 5 lettres ou plus 
+    return [mot for mot in dico_mots if len(mot) >= 5]
+    
+
+dictionnaire = recuperer_dictionnaire()
 
 
-fichier = open("assets\dictionnaire.txt", "r")
-temp = fichier.read()
-dico_mots = temp.split()
-print(dico_mots)
+def choisir_mot(dico_mots):
+    # prend un mot aléatoirement dans une liste de mots
+    n = rd.randint(0, len(dico_mots) - 1)
+    mot = dico_mots[n]
+    return mot
 
+print(choisir_mot(dico_mots=dictionnaire))
