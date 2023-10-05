b97931f16e077ef96708a9311a29a07c5951b7ae
Maxime Barthomeuf <maxime.barthomeuf@gmail.com>
2023-10-05 09:15:12 +0200

    headers

--- fonctions.py
+++ fonctions.py
@@ -1,10 +1,16 @@
-## Olivier et Maxime 
+# -*- coding: utf-8 -*-
 
+"""
+ * @author : maxime et olivier
+ * @email : maxime.barthomeuf@cpe.fr
+ * @date : 05/10/2023, jeudi
+ * TODO :
 
+ Contient les fonctions du programme
+"""
 
 
 fichier = open("assets\dictionnaire.txt", "r")
 temp = fichier.read()
 dico_mots = temp.split()
 print(dico_mots)
-
---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+ * @author : maxime et olivier
+ * @email : maxime.barthomeuf@cpe.fr
+ * @date : 05/10/2023, jeudi
+ * TODO :
+
+ Fonction principale du programme
+"""
